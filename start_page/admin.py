1c19f4f14f7b90cdd6f1f3d633a1f6eb8999ef59
mao13132 <mao13132@gmail.com>
2023-06-29 18:12:18 +0300

    сделал сохранение настроек сайта

--- start_page/admin.py
+++ start_page/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from django.contrib import messages
+
+from .models import Settings
 # Register your models here.
+
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'h1_1', 'h1_2', 'button1', 'button2')
+    list_display_links = ('title', 'description')
+    search_fields = ('title',)
+
+    # def save_model(self, request, obj, form, change):
+    #
+    #
+    #
+    #
+    #     count = Settings.objects.count()
+    #
+    #
+    #     if count >= 1:
+    #         messages.add_message(request, messages.ERROR, 'Нельзя добавить новые настройки. ИЗМЕНЯЙТЕ старые')
+    #     else:
+    #         super(SettingsAdmin, self).save_model(request, obj, form, change)
+
+
+admin.site.register(Settings, SettingsAdmin)
+
--- start_page/models.py
+++ start_page/models.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from django.contrib import messages
+
+
 # Create your models here.
+
+class Settings(models.Model):
+    title = models.CharField(max_length=128, verbose_name='Название сайта')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание сайта')
+    h1_1 = models.CharField(max_length=128, verbose_name='Заголовок 1')
+    h1_2 = models.CharField(max_length=128, verbose_name='Заголовок 2')
+    button1 = models.CharField(max_length=128, verbose_name='Текст кнопки 1')
+    button2 = models.CharField(max_length=128, verbose_name='Текст кнопки 2')
+
+    # def save_model(self, request, obj, form, change):
+    #     if 'owner' in form.changed_data:
+    #         messages.add_message(request, messages.INFO, 'Car has been sold')
+    #     super(Settings, self).save_model(request, obj, form, change)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Settings.objects.exists():
+            Settings.objects.filter(pk=Settings.objects.first().pk).update(title=self.title,
+                                                                           description=self.description, h1_1=self.h1_1,
+                                                                           h1_2=self.h1_2, button1=self.button1,
+                                                                           button2=self.button2)
+        else:
+            return super(Settings, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Настройки'
+        verbose_name_plural = 'Настройки сайта'
